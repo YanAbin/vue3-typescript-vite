c0e1f06ee9771e3c8b9908be3d6da436fb9cfd5c
yanbin-it <yanbin-it@bestpay.com.cn>
2023-04-02 21:13:38 +0800

    build: es、lib打包配置

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -11,13 +11,31 @@ export default defineConfig({
         rollupOptions: {
             //忽略打包vue文件
             external: ["vue", /\.less/],
-            //input: ["index.ts"],
-            output: {
-                globals: {
-                    vue: "Vue",
+            input: ["index.ts"],
+            output: [
+                {
+                  //打包格式
+                  format: 'es',
+                  //打包后文件名
+                  entryFileNames: '[name].mjs',
+                  //让打包目录和我们目录对应
+                  preserveModules: true,
+                  exports: 'named',
+                  //配置打包根目录
+                  dir: '../custom/es'
                 },
-                dir: "dist",
-            }
+                {
+                  //打包格式
+                  format: 'cjs',
+                  //打包后文件名
+                  entryFileNames: '[name].js',
+                  //让打包目录和我们目录对应
+                  preserveModules: true,
+                  exports: 'named',
+                  //配置打包根目录
+                  dir: '../custom/lib'
+                }
+            ]
         },
         lib: {
             entry: "./index.ts",
